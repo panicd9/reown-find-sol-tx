4d5bb7118e90c9b3b012f9b0d1d847d11eb82b79
panicd9 <darkopanic99@gmail.com>
2025-01-29 18:45:42 +0100

    reown integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import DashboardFeature from '@/components/dashboard/dashboard-feature'
+import FindTx from '@/components/find-tx/FindTx'
 
 export default function Page() {
-  return <DashboardFeature />
+  // return <DashboardFeature />
+  return <FindTx />
 }

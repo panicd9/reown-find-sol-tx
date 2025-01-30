7107842ec2b7cd1b6a494ae02b4537f9271d6da1
BuddyAnonymous <tabadzija2@gmail.com>
2025-01-30 03:15:00 +0100

    CSV

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,7 +2,6 @@ import { type NextRequest } from 'next/server'
 import OpenAI from "openai";
 import { schema } from './schema';
 
-const openai = new OpenAI();
 
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
